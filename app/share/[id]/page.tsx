8367c4acc49a0bd986174a97c433c2e6c759785c
nzai <nzaih18@gmail.com>
2023-08-27 14:27:32 +0300

    bugs

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -8,8 +8,6 @@ import { SignInButton } from "@clerk/nextjs";
 import { SiGithub } from "react-icons/si";
 import { Metadata } from "next";
 
-export const runtime = "edge";
-
 type Params = {
   params: {
     id: string;
--- app/ticket/[slug]/page.tsx
+++ app/ticket/[slug]/page.tsx
@@ -7,8 +7,6 @@ import { redirect } from "next/navigation";
 import CopyToClipboard from "@/components/Copy";
 import { Metadata } from "next";
 
-export const runtime = "edge";
-
 export async function generateMetadata(): Promise<Metadata> {
   const getCurrentUser = await currentUser();
   const username = getCurrentUser?.username;
