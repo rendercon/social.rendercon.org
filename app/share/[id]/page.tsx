6fc5f58490401d1068a32f98f3acb84dc7e59436
nzai <nzaih18@gmail.com>
2023-09-05 14:24:22 +0300

    some css stuff

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -35,32 +35,54 @@ export default async function page({ params }: Params) {
   const userBackgroundGradient = user.backgroundGradient || "";
 
   return (
-    <div className="px-4 py-4 sm:px-0 sm:py-0  ">
-      <h1 className="flex justify-center items-center   text-xl lg:text-4xl font-bold font-krona gap-1">
-        <span className="bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 text-transparent bg-clip-text  ">
-          {user.username}
-        </span>{" "}
-        <span className="bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-transparent text-shadow-lg shadow-purple-800 font-bold ">
-          RenderCon
-        </span>
-        Social Card
-      </h1>
-      <HoverCard
-        email={user.email}
-        imageurl={user.imageurl || ""}
-        name={user.name!}
-        number={user.number}
-        username={user.username}
-        gradient={userBackgroundGradient}
-        selectedIcon={user.icon!}
-      />
-      <div className="flex items-center justify-center py-20">
+    <main className=" min-h-screen sm:mx-auto max-w-5xl mx-auto px-4  ">
+      <section className="flex  flex-col-reverse sm:flex-row ">
+        <div className="w-full   ">
+          <HoverCard
+            email={user.email}
+            imageurl={user.imageurl || ""}
+            name={user.name!}
+            number={user.number}
+            username={user.username}
+            gradient={userBackgroundGradient}
+            selectedIcon={user.icon!}
+          />
+        </div>
+        <div className="flex items-center justify-center  ">
+          <p className=" py-4  sm:hidden font-krona text-md whitespace-pre ">
+            <span className=" bg-gradient-to-r from-red-200 via-red-300 to-yellow-200   text-transparent bg-clip-text">
+              {user.username}
+            </span>{" "}
+            <span className=" bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-transparent text-shadow-lg shadow-purple-800  font-bold  ">
+              RenderCon
+            </span>{" "}
+            <span className=" bg-gradient-to-r from-red-200 via-red-300 to-yellow-400 bg-clip-text text-transparent   font-bold  ">
+              Social Card
+            </span>
+          </p>
+          <p className="p-6  hidden sm:block font-krona text-2xl ">
+            <span className="font-krona bg-gradient-to-r from-red-200 via-red-300 to-yellow-200   text-transparent bg-clip-text">
+              {user.username}
+            </span>
+            <br className="" />
+            <span className="text-3xl bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-transparent text-shadow-lg shadow-purple-800   ">
+              RenderCon
+            </span>
+            <br />
+            <span className=" bg-gradient-to-r from-red-200 via-red-300 to-yellow-400 bg-clip-text text-transparent     ">
+              Social Card
+            </span>
+          </p>
+        </div>
+      </section>
+
+      <div className="flex items-center justify-center py-8">
         <SignInButton afterSignInUrl={"/"} mode="redirect">
           <button className="px-3 py-2.5 bg-rendercon-buttons rounded-md inline-flex items-center justify-center sm:w-44 gap-2 outline-none font-semibold font-krona   ">
             get yours <SiGithub />
           </button>
         </SignInButton>
       </div>
-    </div>
+    </main>
   );
 }
