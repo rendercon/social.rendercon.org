e58da437c139d7940f73b61bf84957e9caab59bd
mundume <nzaih18@gmail.com>
2024-09-23 13:10:51 +0300

    fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
+//test deployment
 export const prisma = new PrismaClient().$extends(withAccelerate());
