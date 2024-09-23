17c0337482f0e511cf112d54e571e2eb612f7ebc
mundume <nzaih18@gmail.com>
2024-09-23 12:40:14 +0300

    Remove old content

--- app/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Krona_One } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-export const kronaOne = Krona_One({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-krona",
-});
